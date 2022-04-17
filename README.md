98031905aba5415ca63d740063752e74e81c3f99
William Lea <wjlea93@gmail.com>
2022-04-17 14:42:47 +0100

    add subscriber docs and updated README

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 [![unit-tests Actions Status](https://github.com/WilliamJohnathonLea/go-streams/workflows/unit-tests/badge.svg)](https://github.com/WilliamJohnathonLea/go-streams/actions)
 
 Easily build streamed operations in Go.
+
+## Example stream
+```go
+pub := NewSlicePublisher([]int{0, 1, 2})
+flow := MapFlow(func(i int) string {
+	return fmt.Sprint(i)
+})
+sub := NewSliceSubscriber[string]()
+
+pub.Connect(flow)
+flow.Connect(sub)
+
+stream := New(pub, flow, sub)
+stream.Run()
+```
+
+## Built in Publishers
+- SlicePublisher
+- more to come...
+## Built in Subscribers
+- SliceSubscriber
+- more to come...
--- streams/subscriber.go
+++ streams/subscriber.go
@@ -1,5 +1,9 @@
 package streams
 
+// Subscriber is the termination point of a stream.
+//
+// The type In is the type the Subscriber will receive from the stream.
+// The type Out is the type the Subscriber will produce.
 type Subscriber[In, Out any] interface {
 	StreamExector
 	Inletter[In]
@@ -7,11 +11,14 @@ type Subscriber[In, Out any] interface {
 	Value() Out
 }
 
+// SliceSubscriber is a Subscriber which produces a slice of type T.
 type SliceSubscriber[T any] struct {
 	data []T
 	in   chan T
 }
 
+// NewSliceSubscriber creates a SliceSubscriber and exposes it as
+// a Subscriber.
 func NewSliceSubscriber[T any]() Subscriber[T, []T] {
 	return &SliceSubscriber[T]{
 		data: []T{},
@@ -19,20 +26,25 @@ func NewSliceSubscriber[T any]() Subscriber[T, []T] {
 	}
 }
 
+// Inlet exposes the inbound channel of the SliceSubscriber.
 func (ss *SliceSubscriber[T]) Inlet() chan T {
 	return ss.in
 }
 
+// Subscribe reads the inbound channel and appends the data to
+// the internal slice.
 func (ss *SliceSubscriber[T]) Subscribe() {
 	for x := range ss.in {
 		ss.data = append(ss.data, x)
 	}
 }
 
+// Execute calls Subscribe. It is used when a Stream call its Run method.
 func (ss *SliceSubscriber[T]) Execute() {
 	ss.Subscribe()
 }
 
+// Value returns the slice held in the SliceSubscriber.
 func (ss *SliceSubscriber[T]) Value() []T {
 	return ss.data
 }
